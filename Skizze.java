41bbb02640d8023cd0fb08c5e2d7a938b7a95485
Sabine <10sloe@itg-dah.info>
2018-09-17 10:19:05 +0200

    Gemeinsames Projekt angelegt

---
+++ Skizze.java
@@ -0,0 +1,14 @@
+import javafx.scene.canvas.GraphicsContext;
+
+public class Skizze
+{
+  
+    void draw(GraphicsContext gc){
+
+        gc.fillRect(100,50,100,100);                
+        gc.strokeRect(150,200,100,50);
+
+
+    }
+
+}
